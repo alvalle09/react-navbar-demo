3c273e7eccb2b8e6ba3ec32893487b67acb74c2b
alvalle09 <phredvalle@gmail.com>
2021-08-24 18:58:53 -0500

    navigate to home, about, contact components

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,30 +1,58 @@
 import { Component } from "react";
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container  } from 'react-bootstrap';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+  
+
 
 export default class NavbarComponent extends Component {
     render() {
         return (
-            <div>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </div>
+            <Router>
+                <div>
+                    <Navbar bg="dark" variant={"dark"} expand="lg">
+                        <Container>
+                            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link as={Link} to={"/home"} >Home</Nav.Link>
+                                <Nav.Link as={Link} to={"/about"} >About</Nav.Link>
+                                <Nav.Link as={Link} to={"/contact"} >Contact</Nav.Link>
+                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                </div>
+                <div>                    
+                    <Switch>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/contact">
+                        <Contact/>
+                    </Route>
+                    <Route path="/home">
+                        <Home />
+                    </Route>
+                    </Switch>
+
+                </div>
+            </Router>
         )
     }
 }
